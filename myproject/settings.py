1cc88730a844fadf77b83e1c48b2bfe3c04a9b01
Christian Sunday <potentialsunny47@gmail.com>
2023-01-12 03:23:10 +0100

    whitenoise removed

--- myproject/settings.py
+++ myproject/settings.py
@@ -124,7 +124,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
