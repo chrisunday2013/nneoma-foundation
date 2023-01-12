c726bb03bd42a07838a6648a7508a8695c52a75a
Christian Sunday <potentialsunny47@gmail.com>
2023-01-12 11:12:06 +0100

    connected postgres

--- myproject/settings.py
+++ myproject/settings.py
@@ -71,10 +71,25 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+        
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'WJvwPxAdZRXxYkbMvGDT',
+        'HOST': 'containers-us-west-53.railway.app',
+        'PORT': '6058',
+
         
     }
 }
